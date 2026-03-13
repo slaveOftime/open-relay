09cdc9644b0e2e564a81f4c5c6629fbe626e4f8a
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2026-03-13 10:29:57 +0800

    Bump version to 0.0.6 for homebrew

--- Formula/oly.rb
+++ Formula/oly.rb
@@ -6,21 +6,21 @@
 class Oly < Formula
   desc "Session-persistent PTY daemon for long-running CLI agents"
   homepage "https://github.com/slaveOftime/open-relay"
-  version "0.0.3"
+  version "0.0.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/slaveOftime/open-relay/releases/download/v#{version}/oly-macos-arm64.zip"
       # SHA256 is updated automatically by the release workflow.
-      sha256 "54b0bcc9d90b3f6106faa451d89385d9c7a78049364ab31cfecd85fd4788c6dd"
+      sha256 "19f4d16e7a9014dd4af1a3e9199bd6753856c5f4aa9c2f590eb6a5da2469c40b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/slaveOftime/open-relay/releases/download/v#{version}/oly-linux-amd64.zip"
-      sha256 "bdbf448a78a2863a25a5c04381e339aedbe356e0b92133ac145a9ef589d73256"
+      sha256 "01ff59673cad605a1285395855ce23178d303ff8dd927e10c7a6f154f7d1c073"
     end
   end
 
