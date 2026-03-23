bc2809bef51114f07b5fff0a2af76bbaae58de16
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2026-03-23 11:15:36 +0800

    Add 250ms delay for sending each chunk

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,11 +1,11 @@
 mod attach;
-mod input;
+mod send;
 pub mod join;
 mod list;
 mod logs;
 
 pub use attach::{run_attach, run_attach_node};
-pub use input::run_send;
+pub use send::run_send;
 pub use join::{run_join, run_join_stop};
 pub use list::run_list;
 pub use logs::run_logs;
